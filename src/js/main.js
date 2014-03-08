ec3596460209d4685767bad00c65394a8a31a379
Mark Durrant <mark.j.durrant.gmail.com>
2014-03-08 20:55:23 +0000

    add markers

--- src/js/main.js
+++ src/js/main.js
@@ -11,13 +11,32 @@ $( document ).ready( function() {
 
   var map = L.map('map', {
     zoomControl: false,
-    attributionControl: false,
-    zoom: 7,
-    minZoom: 5,
-    maxZoom: 7
-  }).setView([51.505, -0.09], 13);
+    attributionControl: false
+  }).setView([51.505, -0.09], 5);
 
   L.tileLayer('https://{s}.tiles.mapbox.com/v3/m6-d6.hfccch9n/{z}/{x}/{y}.png', {}).addTo(map);
+
+  var markerGreen = L.divIcon({
+    className: 'marker-green', 
+    iconSize: '60px'
+  });
+
+  var markerOrange = L.divIcon({
+    className: 'marker-orange', 
+    iconSize: '60px'
+  });
+
+  var markerRed = L.divIcon({
+    className: 'marker-red',
+    iconSize: '60px'
+  });
+  // you can set .my-div-icon styles in CSS
+
+  L.marker([52, -1], {icon: markerGreen}).addTo(map);
+
+  L.marker([53, -1], {icon: markerOrange}).addTo(map);
+
+  L.marker([54, -1], {icon: markerRed}).addTo(map);
 
 });
 
