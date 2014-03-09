5b5fe328e3f6891d93effc542dd5318e3728cec6
Marcello Seri <marcello.seri@gmail.com>
2014-03-09 11:53:33 +0000

    Added click event structure

--- src/js/main.js
+++ src/js/main.js
@@ -58,9 +58,18 @@ $( document ).ready( function() {
       } else{
         thisMarker = markerRed;
       }
-      L.marker(latlong, {riseOnHover:'true',title:name,icon: thisMarker}).addTo(map);
+      L.marker(latlong, {riseOnHover:'true',title:name,icon: thisMarker, score:score, url:url}).on('click',wantInfo).addTo(map);
 
     }
   });
+
+  function wantInfo(e) {
+    dataSource = e.target.options
+    name = dataSource.title;
+    url = dataSource.url;
+    score = dataSource.score;
+
+    alert(name + " (" + url + ") => " + score);
+  }
 });
 
